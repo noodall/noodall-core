d90bc7e6a57cf6c52da1a4988d483d744bc70ddc
Daniel Craig <daniel@wearebeef.co.uk>
2010-09-23 14:44:46 +0100

    correct spelling

--- lib/noodall-core.rb
+++ lib/noodall-core.rb
@@ -3,7 +3,7 @@ require 'mongo_mapper_acts_as_tree'
 require 'canable'
 
 module Noodall
-  VESRION = '0.0.1'
+  VERSION = '0.0.1'
   autoload :GlobalUpdateTime,         'noodall/global_update_time'
   autoload :MultiParameterAttributes, 'noodall/multi_parameter_attributes'
   autoload :Search,                   'noodall/search'
