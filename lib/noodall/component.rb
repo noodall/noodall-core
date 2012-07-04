fb1afeea9a470f1258f0605c20bc5ba85bde45fd
Jordan Elver <jordan.elver@gmail.com>
2012-07-04 14:34:10 +0100

    Document Noodall::Component

--- lib/noodall/component.rb
+++ lib/noodall/component.rb
@@ -8,10 +8,20 @@ module Noodall
     embedded_in :node
 
     module ClassMethods
+
+      # Slots this Component can be placed into
+      #
+      # Examples
+      #
+      #   Twitter.possible_slots
+      #   # => [:large, :small, :carousel]
+      #
+      # Returns Array of Symbols for each possible slot
       def possible_slots
         Node.possible_slots
       end
 
+      # Deprecated: Set slots into which this Component is allowed to be placed
       def allowed_positions(*args)
         warn "[DEPRECATION] `allowed_positions` is deprecated. Please use `Noodall::Node.slot` instead."
         allowed_positions = args.reject{|a| !Node.possible_slots.include?(a) }.uniq
@@ -21,10 +31,30 @@ module Noodall
         end
       end
 
+      # List of Component classes that can placed into the supplied slot type
+      #
+      # slot - The type of slot
+      #
+      # Examples
+      #
+      #   Noodall::Component.positions_classes('small')
+      #   # => [Gallery, Quote, Downloads, Twitter, QuickLinks]
+      #
+      # Returns Array of Component classes
       def positions_classes(position)
         Node.send("#{position}_slot_components")
       end
 
+      # List of Component names that can placed into the supplied slot type
+      #
+      # slot - The type of slot
+      #
+      # Examples
+      #
+      #   Noodall::Component.positions_names('small')
+      #   # => ["Gallery", "Quote", "Downloads", "Twitter", "QuickLinks"]
+      #
+      # Returns Array of Component names
       def positions_names(position)
         positions_classes(position).collect{|c| c.name.titleize }
       end
